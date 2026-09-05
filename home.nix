548a308c51802a074ead8f7463488b38f281f3c0
taichi765 <taichi0209.youtub@gmail.com>
2026-09-05 23:14:19 +0900

    Add taplo

--- home.nix
+++ home.nix
@@ -18,10 +18,6 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
@@ -50,6 +46,7 @@
     pkgs.go
     pkgs.nixd
     pkgs.just-lsp
+    pkgs.taplo
     
     # Terminal, Editor
     pkgs.zellij
