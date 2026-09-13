d436d5ac36110840a9b16fb2c9575779d35fdc50
taichi765 <taichi0209.youtub@gmail.com>
2026-09-14 00:09:39 +0900

    Set default editor to helix

--- home.nix
+++ home.nix
@@ -110,7 +110,7 @@
   #  /etc/profiles/per-user/taichi765/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "hx";
   };
 
   # Let Home Manager install and manage itself.
